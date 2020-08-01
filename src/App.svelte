8b65f2f8419f1f1c77aae03407190208147b8f2d
Priyanka R Shah <priyankarshah@Priyankas-MacBook-Pro.local>
2020-08-01 13:50:29 -0700

    made it reactive

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
 	let count = 0;
-
-	function handleClick(){
-		count++
+  $:console.log(`the count is ${count}`)
+	$:if(count>10){
+		alert(`COUNT IS HIGH`);
+		count =9
 	}
+	function handleClick() {
+		count += 1;
+	}
+	
 </script>
 
 <button on:click={handleClick}>
-	Cliked {count}{count===1 ? "time" : "times"}
+	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
 
