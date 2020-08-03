e14d8b950754ad604d50bef90fec44c1ab17a303
Priyanka R Shah <priyankarshah@Priyankas-MacBook-Pro.local>
2020-08-02 22:04:35 -0700

    if else block

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	let count = 0;
-  $:console.log(`the count is ${count}`)
+  	$:console.log(`the count is ${count}`)
 	$:if(count>10){
 		alert(`COUNT IS HIGH`);
 		count =9
@@ -8,10 +8,25 @@
 	function handleClick() {
 		count += 1;
 	}
-	
+
+	let user = { loggedIn: false };
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
 </script>
 
 <button on:click={handleClick}>
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+
+{#if user.loggedIn}
+	<button on:click={toggle}>
+		Log out
+	</button>
+{:else}
+	<button on:click={toggle}>
+		Log in
+	</button>
+{/if}
 
