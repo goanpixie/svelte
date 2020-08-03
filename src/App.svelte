9a68fe34535436561e404bb6d7d9af47efe40329
Priyanka R Shah <priyankarshah@Priyankas-MacBook-Pro.local>
2020-08-02 22:14:39 -0700

    if else block

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,8 @@
 	function toggle() {
 		user.loggedIn = !user.loggedIn;
 	}
+
+	let x=9;
 </script>
 
 <button on:click={handleClick}>
@@ -28,5 +30,15 @@
 	<button on:click={toggle}>
 		Log in
 	</button>
+{/if}
+
+{#if x > 10}
+	<p>{x} is greater than 10</p>
+{:else}
+	{#if 5 > x}
+		<p>{x} is less than 5</p>
+	{:else}
+		<p>{x} is between 5 and 10</p>
+	{/if}
 {/if}
 
