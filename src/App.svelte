fca02199112b6cd7a8a830572921a100d975cecd
Priyanka R Shah <priyankarshah@Priyankas-MacBook-Pro.local>
2020-08-02 22:27:38 -0700

    each block

--- src/App.svelte
+++ src/App.svelte
@@ -16,6 +16,13 @@
 	}
 
 	let x=9;
+
+	//each block 
+	let cats = [
+	{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+	{ id: 'z_AbfPXTKms', name: 'Maru' },
+	{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+];
 </script>
 
 <button on:click={handleClick}>
@@ -41,4 +48,17 @@
 		<p>{x} is between 5 and 10</p>
 	{/if}
 {/if}
+
+
+
+
+<h1>The Famous Cats of YouTube</h1>
+
+<ul>
+	{#each cats as { id, name }, i}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={id}">
+			{i + 1}: {name}
+		</a></li>
+	{/each}
+</ul>
 
